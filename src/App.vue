c8785eea98530c1ea967f07815cc56ab17d5e3c4
amorgan <adam.morgan013@gov.wales>
2018-06-18 13:41:49 +0100

    Adding fontawesome, bootstrap vue and loaders dependencies to improve UI

--- src/App.vue
+++ src/App.vue
@@ -1,63 +1,96 @@
 <template>
   <main id="app">
+
+    <transition name="fade">
+      <div class="loader" v-if="loading">
+        <div class="container h-100">
+          <div class="row h-100 justify-content-center align-items-center">
+            <bounce-loader :loading="loading" :color="color"></bounce-loader>
+            <!-- <p>Loading...</p> -->
+          </div>
+        </div>
+      </div>
+    </transition>
+
     <div class="container">
       <div class="row justify-content-md-center">
-        <div class="col-md-8 col-sm-12">
+        <div class="col-md-10 col-sm-12">
 
-          <form class="mb-4" method="get" @submit.prevent="getStats">
-            <div v-for="(player, index) in players" :key="index">
-              <div class="form-group row">
-                <label class="col-sm-4 col-form-label" :for="'player' + index">Player {{ index + 1 }}</label>
-                <div class="col-sm-8">
-                  <input :class="{ 'is-invalid' : player.error }" class="form-control ml-md" :id="'player' + index" :name="'player' + index" type="text" placeholder="e.g. Ninja" v-model="player.username">
-                  <div v-if="player.error" class="invalid-feedback">
-                    This username does not exist for this platform.
-                  </div>
-                </div>
-              </div>
-              <fieldset class="form-group">
-                <div class="row">
-                  <legend class="col-form-label col-sm-4 pt-0">Platform</legend>
-                  <div class="col-sm-8">
-                    <div class="form-check form-check-inline">
-                      <input class="form-check-input" type="radio" :id="index + 'PC'" value="pc" v-model="player.platform">
-                      <label class="form-check-label" :for="index + 'PC'">PC</label>
-                    </div>
-                    <div class="form-check form-check-inline">
-                      <input class="form-check-input" type="radio" :id="index + 'PS4'" value="psn" v-model="player.platform" checked="checked">
-                      <label class="form-check-label" :for="index + 'PS4'">PS4</label>
-                    </div>
-                    <div class="form-check form-check-inline">
-                      <input class="form-check-input" type="radio" :id="index + 'Xbox'" value="xbl" v-model="player.platform" checked="checked">
-                      <label class="form-check-label" :for="index + 'Xbox'">Xbox</label>
-                    </div>
-                  </div>
-                </div>
-              </fieldset>
+          <div class="card mt-5">
+            <div class="card-header">
+              <strong>Fortnite Player Comparison</strong>
             </div>
-            <!-- Form Buttons -->
-            <button v-if="comparePlayers" type="submit" class="btn btn-primary btn-sm mr-2">Compare players</button>
-            <button v-if="players.length < 4" @click="addPlayer" type="button" class="btn btn-success btn-sm mr-2">Add player</button>
-            <button v-if="players.length > 2" @click="removePlayer" type="button" class="btn btn-danger btn-sm">Remove player</button>
-          </form>
+            <div class="card-body">
+              <form method="get" @submit.prevent="getStats">
+                <div v-for="(player, index) in players" :key="index">
+                  <div class="form-group row">
+                    <label class="col-sm-4 col-form-label" :for="'player' + index">Player {{ index + 1 }}</label>
+                    <div class="col-sm-8">
+                      <input :class="{ 'is-invalid' : player.error }" class="form-control ml-md" :id="'player' + index" :name="'player' + index" type="text" placeholder="e.g. Ninja" v-model="player.username">
+                      <div v-if="player.error" class="invalid-feedback">
+                        This username does not exist for this platform.
+                      </div>
+                    </div>
+                  </div>
+                  <fieldset class="form-group">
+                    <div class="row">
+                      <legend class="col-form-label col-sm-4 pt-0">Platform</legend>
+                      <div class="col-sm-8">
+                        <div class="form-check form-check-inline">
+                          <input class="form-check-input" type="radio" :id="index + 'PC'" value="pc" v-model="player.platform">
+                          <label class="form-check-label" :for="index + 'PC'">PC</label>
+                        </div>
+                        <div class="form-check form-check-inline">
+                          <input class="form-check-input" type="radio" :id="index + 'PS4'" value="psn" v-model="player.platform" checked="checked">
+                          <label class="form-check-label" :for="index + 'PS4'">PS4</label>
+                        </div>
+                        <div class="form-check form-check-inline">
+                          <input class="form-check-input" type="radio" :id="index + 'Xbox'" value="xbl" v-model="player.platform" checked="checked">
+                          <label class="form-check-label" :for="index + 'Xbox'">Xbox</label>
+                        </div>
+                      </div>
+                    </div>
+                  </fieldset>
+                </div>
+                <!-- Form Buttons -->
+                <button type="submit" class="btn btn-primary btn-sm mr-2" :disabled="comparePlayers == 0">Compare</button>
+                <button @click="addPlayer" type="button" class="btn btn-success btn-sm mr-2" :disabled="players.length > 3"><icon icon="plus" /></button>
+                <button @click="removePlayer" type="button" class="btn btn-danger btn-sm" :disabled="players.length < 3"><icon icon="minus" /></button>
+              </form>
+            </div>
+          </div>
 
           <transition name="fade">
-            <div class="alert alert-info" role="alert" v-if="loading">
+            <div class="alert alert-info mt-4" role="alert" v-if="loading">
               Loading.....
             </div>
           </transition>
 
           <transition name="fade">
-            <div class="alert alert-danger" role="alert" v-if="error">
+            <div class="alert alert-danger mt-4" role="alert" v-if="error">
               An error has occurred.
             </div>
           </transition>
 
-          <div v-if="lifeTimeChart">
+          <div v-if="lifeTimeChart" class="mt-4">
             <transition name="fade">
               <canvas id="lifetime-chart"></canvas>
             </transition>
           </div>
+
+          <!-- <b-card no-body>
+            <b-tabs pills fill card>
+              <b-tab title="Single" active>
+                Tab Contents 1
+              </b-tab>
+              <b-tab title="Duos">
+                Tab Contents 2
+              </b-tab>
+              <b-tab title="Squads">
+                Tab Contents 2
+              </b-tab>
+            </b-tabs>
+          </b-card> -->
 
         </div>
       </div>
@@ -73,6 +106,7 @@
     name: "app",
     data() {
       return {
+        color: '#ffffff',
         loading: false,
         error: false,
         players: [
@@ -136,7 +170,7 @@
             if (this.error === false) {
               // Start formatting the lifetime stats chart
               this.lifeTimeChart.type = 'horizontalBar';
-              this.lifeTimeChart.data.labels = ['Matches Played', 'Wins', 'Win %', 'Kills', 'K/D'];
+              this.lifeTimeChart.data.labels = ['Matches Played'];
 
               // Map the data from the api request
               this.lifeTimeChart.data.datasets = results.map(result => {
@@ -144,10 +178,10 @@
                   label: result.data.epicUserHandle,
                   data: [
                     result.data.lifeTimeStats[7].value,
-                    result.data.lifeTimeStats[8].value,
-                    parseInt(result.data.lifeTimeStats[9].value.replace('%', ''), 10),
-                    result.data.lifeTimeStats[10].value,
-                    parseInt(result.data.lifeTimeStats[11].value, 10),
+                    // result.data.lifeTimeStats[8].value,
+                    // parseInt(result.data.lifeTimeStats[9].value.replace('%', ''), 10),
+                    // result.data.lifeTimeStats[10].value,
+                    // parseInt(result.data.lifeTimeStats[11].value, 10),
                   ],
                   borderWidth: 3
                 };
@@ -159,17 +193,22 @@
                 responsive: true,
                 lineTension: 1,
                 scales: {
-                  yAxes: [{
+                  xAxes: [{
+                    gridLines: {
+                      display: false,
+                      drawBorder: false
+                    },
                     ticks: {
-                      beginAtZero: true,
-                      padding: 25,
+                      display: false
+                    }
+                  }],
+                  yAxes: [{
+                    gridLines: {
+                      display: false,
+                      drawBorder: false
                     }
                   }]
                 },
-                title: {
-                  display: true,
-                  text: 'Lifetime Stats'
-                }
               };
 
               // Create the chart
@@ -206,19 +245,29 @@
 </script>
 
 <style>
+  body {
+    background-color: #fbfbfb !important;
+  }
+
   #app {
     font-family: "Source Sans Pro", "Helvetica Neue", Arial, sans-serif;
     -webkit-font-smoothing: antialiased;
     -moz-osx-font-smoothing: grayscale;
-    color: #2c3e50;
-    margin-top: 60px;
   }
 
   .fade-enter-active,
   .fade-leave-active {
     transition: opacity 0.5s;
   }
-  .fade-enter, .fade-leave-to /* .fade-leave-active below version 2.1.8 */ {
+  .fade-enter, .fade-leave-to {
     opacity: 0;
+  }
+
+  .loader {
+    position: fixed;
+    height: 100%;
+    width: 100%;
+    background: linear-gradient(to bottom right, #339BDC, #7ADDF8);
+    z-index: 2;
   }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,13 +1,29 @@
 import Vue from 'vue'
 import App from './App.vue'
-import RecentMatches from './components/RecentMatches.vue'
-import 'bootstrap/dist/css/bootstrap.css'
+import BootstrapVue from "bootstrap-vue";
+import RecentMatches from './components/RecentMatches.vue';
+import BounceLoader from "vue-spinner/src/BounceLoader.vue";
 
-// Allows moment js to be used
+// Use dependencies
+Vue.use(BootstrapVue);
 Vue.use(require('vue-moment'));
+
+// Import bootstrap css
+import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap-vue/dist/bootstrap-vue.css";
+
+// Import font awesome
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { faPlus, faMinus } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+
+library.add(faPlus);
+library.add(faMinus);
 
 // Components
 Vue.component('recent-matches', RecentMatches);
+Vue.component("icon", FontAwesomeIcon);
+Vue.component("bounce-loader", BounceLoader);
 
 Vue.config.productionTip = false
 
